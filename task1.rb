a43dec312e23cdeeeb076c60c3422adfb6fe9380
Alexey Maisuradze <ama@it-keeper.ru>
2018-06-11 20:32:59 +0400

    Update task1.rb

--- task1.rb
+++ task1.rb
@@ -1,9 +1,4 @@
 def delRKN(strToDel)
-  chrCPDel = Array.new
-  chrCPDel = [ 1088, 1085, 1082, 1056, 1053, 1050 ]
-  strCPToDel = strToDel.to_s.codepoints.to_a 
-  strBToRet = strCPToDel - chrCPDel
-  return strBToRet.pack('U*')
+  return (strToDel.to_s.codepoints.to_a - [ 1088, 1085, 1082, 1056, 1053, 1050 ]).pack('U*') 
 end
-
 puts delRKN(ARGV[0])
